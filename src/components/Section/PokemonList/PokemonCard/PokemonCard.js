c381db8013eab2bb86f55d836928dd980f00d14f
Lukasz <bobryk.lukasz@gmail.com>
2020-04-10 11:16:25 +0200

    creating basic page with working pokemon list took from pokeApi. still working on pagination

--- src/components/Section/PokemonList/PokemonCard/PokemonCard.js
+++ src/components/Section/PokemonList/PokemonCard/PokemonCard.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-const PokemonCard = () => {
+const PokemonCard = (props) => {
+
+    const [pokemon, setPokemon] = useState(null)
+
+    function fetchPokemon(){
+        fetch(`${props.pokeUrl}`)
+        .then(response => response.json())
+        .then(pokemon => setPokemon(pokemon))
+      }
+
+    useEffect(() => {
+        fetchPokemon();
+    }, []);
 
     return (
-        <div className="col-sm-6 col-md-3 my-5">
+        <div className="col-sm-6 col-md-3 my-3">
             <div className="card">
                 <div className="card-header">
-                    <h1>Pokemon TEST</h1>
+                    {pokemon === null ? (
+                        <h1>Trwa ładowanie pokemona</h1>
+                    ) : ( <>
+                        <h5>{pokemon.name}</h5>
+                        <img src={pokemon.sprites.front_default} />
+                        </>
+                    )}
                 </div>
             </div>
         </div>
--- src/components/Section/PokemonList/PokemonList.js
+++ src/components/Section/PokemonList/PokemonList.js
@@ -3,28 +3,46 @@ import PokemonCard from './PokemonCard/PokemonCard'
 
 const PokemonList = () => {
 
+    const [limit, setLimit] = useState(20)
+    const [offset, setOffset] = useState(0)
+    const [apiUrl, setApiUrl] = useState(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
     const [allPokemon, setAllPokemon] = useState(null)
 
+    console.log(apiUrl.count)
+
     function fetchPokemon(){
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=5')
+        fetch(apiUrl)
         .then(response => response.json())
         .then(pokeList => setAllPokemon(pokeList.results))
       }
 
-    fetchPokemon()
-      
+    useEffect(() => {
+        fetchPokemon();
+    }, []);
 
-    return (
-        <div className="row mx-auto">
+    return ( <>
+        <div className="row">
             {allPokemon === null ? (
                 <h1>Trwa ładowanie pokemonów</h1>
             ) : (
-                <div>{allPokemon.map((e, i)=> <h1>test</h1>)}</div>
+                <>{allPokemon.map((e, i)=> <PokemonCard key={i} pokeUrl={e.url} />)}</>
+                
             )}
-            
-
         </div>
-    )
+        <div className="row">
+            {allPokemon === null ? (null) : (
+                <nav aria-label="Page navigation example" className="mx-auto">
+                    <ul class="pagination">
+                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                    </ul>
+                </nav>
+            )}
+        </div>
+    </>)
 }
 
 export default PokemonList
